25570c2788c52dcc5790014fce77d7dd550d052c
Aditya kumar <yomecool@gmail.com>
2023-04-30 14:37:57 -0400

    NN-Generalization

--- Code/Backpropagation-Generalization.py
+++ Code/Backpropagation-Generalization.py
@@ -116,6 +116,45 @@ class Generalized_NeuralNetwork_Backpropagation:
                     setattr(self, f"b{i+1}", self.b_list[i])
                 index += 1
             self.epoch_error[epoch] = error.T**2
+     def batch_train(self,train_data,target,learning_rate=0.1,epochs=750):
+        alpha = learning_rate
+        epochs = epochs
+        # self.NNOP = np.empty((len(train_data),self.neurons[-1]))
+        # self.t_plot = np.empty((len(train_data),self.neurons[-1]))
+        self.epoch_error = np.empty((epochs,len(train_data),self.neurons[-1]))
+        for epoch in range(epochs):
+            error = np.empty((self.neurons[-1], len(train_data)))
+            zipped = list(zip(train_data,target))
+            np.random.shuffle(zipped)
+            input, output = zip(*zipped)
+            index = 0
+            for p,t in zip(input,output):
+                n = {i + 1: None for i in range(len(self.activation_list))}
+                n[1] = np.dot(self.w1, np.array(p).reshape(len(p), 1)) + self.b1
+                a = {i: None for i in range(len(self.activation_list)+1)}
+                a[0] = np.array(p).reshape(len(p), 1)
+                a[1] = self.activation_choice(self.activation_list[0], n[1])
+                for i in range(len(self.activation_list) - 1):
+                    n[i+2] = np.dot(getattr(self,f"w{i+2}"), a[i + 1]) + getattr(self,f"b{i+2}")
+                    a[i+2] = self.activation_choice(self.activation_list[i + 1], n[i + 2])
+                # self.NNOP[index] = a[list(a)[-1]].ravel()
+                # self.t_plot[index] = t.ravel()
+                error[:,index] = np.ravel(np.array(t).reshape(len(t),1)-a[list(a)[-1]])
+                s = {i:None for i in range(1,len(self.activation_list)+1)}
+                F_n_last = self.activation_choice(self.activation_list[-1],n[list(n)[-1]],derivative=True)
+                Fn = np.diag([element for row in F_n_last for element in row])
+                s[list(s)[-1]] = -2 * np.dot(Fn,error[:,index].reshape(self.neurons[-1],1))
+                for i in range(len(self.activation_list)-1):
+                    F_n = self.activation_choice(self.activation_list[-2-i], n[list(n)[-2-i]], derivative=True)
+                    Fn_ = np.diag([element for row in F_n for element in row])
+                    s[list(s)[-2-i]] = np.dot(np.dot(Fn_,self.w_list[-1-i].T),s[list(s)[-1-i]])
+                for i in range(len(self.w_list)):
+                    self.w_list[i] = self.w_list[i] - alpha*np.dot(s[list(s)[i]],a[i].T)
+                    setattr(self,f"w{i+1}",self.w_list[i])
+                    self.b_list[i] = self.b_list[i] - alpha*s[list(s)[i]]
+                    setattr(self, f"b{i+1}", self.b_list[i])
+                index += 1
+            self.epoch_error[epoch] = error.T**2
 
     def prediction(self,input):
         output = np.empty((len(input),self.neurons[-1]))
@@ -146,7 +185,6 @@ class Generalized_NeuralNetwork_Backpropagation:
         plt.legend()
         plt.tight_layout()
         plt.show()
-
 
 # Feedforward test
 network = Generalized_NeuralNetwork_Backpropagation([1,10,1],['sigmoid','linear'])
--- Code/Observations.md
+++ Code/Observations.md
@@ -1,9 +1,12 @@
 # Batch vs Online Training
 * It seems like batch training takes up longer time to converge. This is possibly because of the lesser number of weight updates within each epoch. 
-* However, batch training does converge in lesser number of total weight updates as compared to incremental training. 
-* SSE is not declining as smoothly as expected with batch gradient (for small batch) as we would expect. The SSE does not seem to flatline either. Ran the code for 10,000 epochs. 
-* For full batch training, the algorithm doesn't seem to converge although the SSE flatlines after certain number of epochs. This result is not expected. With full batch, the expected gradient direction should converge toward minima. 
+* However, batch training does converge in lesser number of total weight updates as compared to incremental training (tested with a small batch size of 5, total number of weight updates for network to approximate function was slightly lesser than incremental updates.)
+* SSE is not declining as smoothly as expected with batch gradient (for small batch) as we would expect. The SSE does not seem to flatline either.  
+* For full batch training, the algorithm doesn't seem to converge although the SSE flatlines after certain number of epochs. This result is not expected. With full batch, the expected gradient direction should converge toward minima. Ran the code for 10,000 epochs.
+* (Update: with full batch training, the sse is always a smooth curve. However, due to pc limitations, it seems like to achieve a comparable number of weight updates, the number of epochs need to be increased drastically which is not possible with CPU. This was tested on GPU of google cloud and the function was approximated by increasing number of epochs to 20,000)
+* For reference, incremental training seems to converge in 1000 epochs = 1000 x 100 (Input samples) = 100,000 weight updates. With batch training. 20,000 weight updates seem to approximate the function well.
+
 
 # Neural Network Generaliziation
 * Successfully completed generalizing stochastic gradient training to custom number of layers and custom number of transfer functions.
-* Hurdles faced: Error calculation and hence sensitivity propagation was incorrect for the longest time which was causing my weights and biases to explode and activation output to overflow. Helpful note: if weights and biases seem to overflow, there is a problem with sensitive backpropagation. This should be checked first. Additionally, error calculation should be checked.
+* Hurdles faced: Error calculation and hence sensitivity propagation was incorrect for the longest time which was causing my weights and biases to explode and activation output to overflow. Helpful note: if weights and biases seem to explode, there is a problem with sensitivity backpropagation. This should be checked first. Additionally, error calculation should be checked.
